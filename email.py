76e7d955d3c6c8c1e3dd3f124d0dee27db4892c0
skrishnan99 <skrishnan99@icloud.com>
2019-04-21 18:32:28 -0700

    Initial set up

---
+++ email.py
@@ -0,0 +1,13 @@
+import yagmail
+
+receiver = "your@gmail.com"
+body = "Hello there from Yagmail"
+filename = "document.pdf"
+
+yag = yagmail.SMTP("my@gmail.com")
+yag.send(
+    to=receiver,
+    subject="Yagmail test with attachment",
+    contents=body, 
+    attachments=filename,
+)
